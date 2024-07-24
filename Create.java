1eef22bf0f17f32fdfa689fc062e93e0e0f931e5
KOushikguptha <koushikguptha2003@gmail.com>
2024-07-24 22:59:45 +0530

    Linked List

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
-public class Create{
-public static void main(String[]args){
-// create  a Linked List class object
-  // Add Values in it
-  // Iterate it by using Iterator
-}
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class Create {
+    public static void main(String[] args) {
+        // Create a LinkedList class object
+        LinkedList<String> list = new LinkedList<>();
+
+        // Add values in it
+        list.add("Yash");
+        list.add("Hemanth");
+        list.add("Manoj");
+
+        // Iterate it by using Iterator
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String value = iterator.next();
+            System.out.println(value);
+        }
+    }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,18 @@
-public class ForL{
-  public static void main(String[]args){
-  // create a Linked List Object
-    // Add Values
-    // iterate it by using For Each
-  }}
+import java.util.LinkedList;
+
+public class ForL {
+    public static void main(String[] args) {
+        // Create a LinkedList object
+        LinkedList<String> list = new LinkedList<>();
+
+        // Add values
+        list.add("Yash");
+        list.add("Hemanth");
+        list.add("Manoj");
+
+        // Iterate it by using for-each loop
+        for (String string : list) {
+            System.out.println(string);
+        }
+    }
+}
