b4227fe562d7a0f9f7a383119ef0c53c168de3ac
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-07-24 13:41:57 +0530

    linkedlist

--- Dynamic.java
+++ Dynamic.java
@@ -1,9 +1,18 @@
+import java.util.LinkedList;
+import java.util.Iterator;
 public class Dynamic{
 public static void main(String[]args)
   {
-  // create an Object for LinkedList
-    // add person objects in it
-    // iterate that List using iterator
-    // print it by using while loop
+    LinkedList<Person> list=new LinkedList<Person>();
+    list.add(new Person("Manoj",21,101));
+    list.add(new Person("Krishna",22,102));
+    list.add(new Person("Lakshman",23,103));
+    Iterator<Person> it=list.iterator();
+    
+    while(it.hasNext()){
+      System.out.println(it.next().toString());
+    }
+
+  
   }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,16 @@
+import java.util.LinkedList;
+
 public class ForL{
   public static void main(String[]args){
-  // create a Linked List Object
-    // Add Values
-    // iterate it by using For Each
+
+  LinkedList<String> list=new LinkedList<String>();
+  list.add("Manoj");
+  list.add("Krishna");
+  list.add("Lakshman");
+  for (String s : list) {
+    System.out.println(s);
+    
+  }
+  
+  
   }}
