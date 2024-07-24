56692760c693b02b8e13b459300955460acdc5e0
ranjith81869 <ranjith81869@email.com>
2024-07-24 18:19:32 +0530

    LinkedLists-6.0

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  LinkedList<Integer> list = new LinkedList<Integer>();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  list.add(4);
+  list.add(5);
+  list.add(6);
+  Iterator<Integer> itr = list.iterator();
+  while(itr.hasNext()){
+    System.out.println(itr.next());
+  }
 }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Dynamic{
 public static void main(String[]args)
   {
@@ -5,5 +8,12 @@ public static void main(String[]args)
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+    LinkedList<person> list = new LinkedList<person>();
+    list.add(new person(24, "Vishal", "B+", "Male", "Delhi"));
+    Iterator<person> itr = list.iterator();
+    while(itr.hasNext())
+      {
+      System.out.println(itr.next());
+      }
   }
 }
