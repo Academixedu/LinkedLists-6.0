56ddecc3eacdb11f75c389e77f527a5cf1376e93
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 12:21:05 +0530

    Create Dynamic.java

---
+++ Dynamic.java
@@ -0,0 +1,9 @@
+public class Dynamic{
+public static void main(String[]args)
+  {
+  // create an Object for LinkedList
+    // add person objects in it
+    // iterate that List using iterator
+    // print it by using while loop
+  }
+}
