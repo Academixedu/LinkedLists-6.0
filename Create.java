64ea36e7f0b90ed9f12b75c9a508bfe3ac034c57
Ro-045 <pratapnaidu016@gmail.com>
2024-07-24 20:39:23 +0530

    pratap

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
+import java.util.LinkedList;
+
+import java.util.Iterator;
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  LinkedList<Integer> l=new LinkedList<Integer>();
+  l.add(12);
+  l.add(14);
+  l.add(16);
+  l.remove(2);
+  l.add(18);
+  Iterator i=l.iterator();
+  while (i.hasNext()) {
+    System.out.print(i.next()+"\t");
+}
 }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,28 @@
-public class ForL{
-  public static void main(String[]args){
-  // create a Linked List Object
-    // Add Values
-    // iterate it by using For Each
-  }}
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class ForL {
+  LinkedList<Long> l = new LinkedList<>();
+ 
+  public void insert(long mobile) {
+   
+      l.add(mobile);
+      System.out.println(l);
+    }
+  public static void main(String[] args) {
+    ForL forL = new ForL();
+    Scanner sc = new Scanner(System.in);
+
+    // Adding Values
+    forL.insert(8967543560L);
+    forL.insert(7652349082L);
+    forL.insert(6793245685L);
+
+    // Iterating over the list using For Each
+    for (Long mobile : forL.l) {
+      System.out.println(mobile);
+    }
+
+    sc.close();
+  }
+}
