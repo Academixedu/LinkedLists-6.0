3367e056ac24b13caeb832e5e5fd4d5c3d91dc88
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-07-26 00:01:22 +0530

    done

--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
 public class Dynamic{
 public static void main(String[]args)
   {
@@ -5,5 +8,14 @@ public static void main(String[]args)
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+    LinkedList<person> list=new LinkedList<person>();
+    list.add(new person("Sri", 21));
+    list.add(new person("pavan", 22));
+    list.add(new person("kumar", 23));
+    Iterator<person> it=list.iterator();
+    while(it.hasNext())
+    {
+    System.out.println(it.next());
+    }
   }
 }
