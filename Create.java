72fcc77c8c4aeb4e22c4b2dd09992743628a466b
siva-245711 <sivanagendra809@gmail.com>
2024-07-25 22:45:43 +0530

    Linkedlist

--- Create.java
+++ Create.java
@@ -1,7 +1,27 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  LinkedList<Integer> obj =new LinkedList<Integer>();
+  obj.add(1);
+  obj.add(2);
+  obj.add(3);
+  obj.add(4);
+  obj.add(5);
+  obj.add(6);
+  obj.add(7);
+  obj.add(8);
+  obj.add(9);
+  obj.add(10);
+  Iterator load=obj.listIterator();
+  while(load.hasNext())
+  {
+    System.out.println(load.next());
+  }
 }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,7 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Dynamic{
 public static void main(String[]args)
   {
@@ -5,5 +9,17 @@ public static void main(String[]args)
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+    LinkedList<persons> list = new LinkedList<persons>();
+    persons p1 = new persons("siva", 24, "Delhi", "Male");
+    persons p2 = new persons("shiva", 21, "kkd", "Male");
+    list.add(p1);
+    list.add(p2);
+    Iterator load=list.listIterator();
+    while(load.hasNext())
+    {
+      System.out.println(load.next());
+
   }
-}
+}}
+
+    
