70ca5e8a1b2f6ccca81ae4239a2254a4eb468e98
swapna-321 <swapna@gmail.com>
2024-07-26 15:10:51 +0530

    linkedin

--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Iterator;
 public class Dynamic{
 public static void main(String[]args)
   {
@@ -5,5 +7,12 @@ public static void main(String[]args)
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+    LinkedList<Person> list=new LinkedList<>();
+    list.add(new Person("swapna",21));
+    list.add(new Person("soppi",21));
+    Iterator it =list.iterator();
+    while(it.hasNext()){
+      System.out.println(it.next());
+    }
   }
 }
