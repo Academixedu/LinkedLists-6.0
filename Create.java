4c604e79f643546201585ff7f979c3933dacfe02
Naveentogaru1 <togarunaveen1@gmail.com>
2024-07-26 22:27:40 +0530

    LinkedLists-6.0/Create.java

--- Create.java
+++ Create.java
@@ -1,5 +1,17 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
+  LinkedList<Integer> List = new LinkedList<Integer>();
+  List.add(1);
+  List.add(2);
+  List.add(3);
+  Iterator<Integer> i=List.iterator();
+  while(i.hasNext()){
+      System.out.println(i.next());
+  }
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
