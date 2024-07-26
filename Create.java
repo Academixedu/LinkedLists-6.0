609973873c506e86e7d3920249df74c8f4057c1e
urstrulysmohana <urstrulysmohan@gmail.com>
2024-07-26 20:36:09 +0530

    mohan

--- Create.java
+++ Create.java
@@ -1,7 +1,19 @@
-public class Create{
-public static void main(String[]args){
-// create  a Linked List class object
-  // Add Values in it
-  // Iterate it by using Iterator
-}
+import java.util.LinkedList;
+import java.util.Iterator;
+
+ class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("apple");
+        list.add("banana");
+        list.add("orange");
+
+        Iterator<String> iterator = list.iterator();
+        
+        while (iterator.hasNext()) {
+            String day = iterator.next();
+            System.out.println(day);
+        }
+    }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,9 +1,38 @@
-public class Dynamic{
-public static void main(String[]args)
-  {
-  // create an Object for LinkedList
-    // add person objects in it
-    // iterate that List using iterator
-    // print it by using while loop
-  }
+import java.util.LinkedList;
+import java.util.Iterator;
+
+class Person {
+    private String name;
+    private int age;
+
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+ class LinkedListPersonExample {
+    public static void main(String[] args) {
+        LinkedList<Person> people = new LinkedList<>();
+
+        people.add(new Person("mohan", 20));
+        people.add(new Person("krishna", 25));
+        people.add(new Person("hari", 15));
+
+        Iterator<Person> iterator = people.iterator();
+        
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
+        }
+    }
 }
