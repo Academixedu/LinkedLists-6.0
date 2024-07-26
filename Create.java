4a1a7cae5d012494025196bedfd0520f0905e233
manicharanchindam <manicharanchindam@email.com>
2024-07-26 20:45:42 +0530

    linked list done

--- Create.java
+++ Create.java
@@ -1,7 +1,18 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
-// create  a Linked List class object
-  // Add Values in it
-  // Iterate it by using Iterator
+ LinkedList <Integer> linkedList=new LinkedList<>();
+ linkedList.add(1);
+ linkedList.add(2);
+ linkedList.add(3);
+ linkedList.add(4);
+  @SuppressWarnings("rawtypes")
+  Iterator i = linkedList.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+
 }
 }
