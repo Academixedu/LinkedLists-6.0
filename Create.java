200002a5fe64e3bbff6181e8b97cc282a629eb31
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-12 19:44:19 +0530

    linklist

--- Create.java
+++ Create.java
@@ -1,7 +1,27 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
+
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  LinkedList<Integer> l=new LinkedList<>();    
+
+
+l.add(23);
+l.add(12);
+l.add(6);
+Iterator i=l.iterator();
+while(i.hasNext()){
+    System.out.println(i.next());
 }
+
+}   
 }
+
+
+
+
