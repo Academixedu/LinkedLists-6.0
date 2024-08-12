3f84f0f3a80c637f1add2e24ab142790ac441e74
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 18:17:22 +0530

    Update ForL.java

--- ForL.java
+++ ForL.java
@@ -1,6 +1,20 @@
-public class ForL{
-  public static void main(String[]args){
-  // create a Linked List Object
-    // Add Values
-    // iterate it by using For Each
-  }}
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class prac{
+    public static void main(String[]args){
+    // create a Linked List Object
+      // Add Values
+      // iterate it by using For Each
+       LinkedList<Integer> s=new LinkedList<>();
+       s.add(9);
+       s.add(02);
+       s.add(02);
+       Iterator i=s.iterator();
+       for(Integer e:s){
+        System.out.println(e);
+
+       }
+
+    }
+}
