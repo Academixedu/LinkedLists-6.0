83b7c69c9c0125a546a2d6f1eac1729c827c9b65
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-12 18:00:02 +0530

    from fayaz

--- ForL.java
+++ ForL.java
@@ -1,6 +1,15 @@
+import java.util.LinkedList;
+
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
+    LinkedList<Integer> l = new LinkedList<>();
+    l.add(1);
+    l.add(2);
+    l.add(3);
+    for(Integer i:l){
+      System.out.println(i);
+    }
   }}
