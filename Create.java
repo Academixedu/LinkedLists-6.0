e39e69125b47c3bbfc69b9f408d8ed8ef0b7e507
2110Abhishek <agchoudhari2110@gmail.com>
2024-08-12 19:41:09 +0530

    Abhishek

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
+  import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+ LinkedList<Object> l=new LinkedList<>();
+ l.add(1);
+ l.add(2);
+ l.add(3);
+ l.add(0, "Java");
+ l.add(2,"is");
+ Iterator <Object> i=l.iterator();
+ while (i.hasNext()) {
+  System.out.print(i.next() + " ");
+ }
+
 }
 }
