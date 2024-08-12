f40e207284b243022b236b6c5fb2e502c49d0d99
sujatha-94 <sujathakarri701@gmail.com>
2024-08-12 19:22:09 +0530

    lists

--- Dynamic.java
+++ Dynamic.java
@@ -1,9 +1,33 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Dynamic{
+  String name;
+  int age;
+  String gender;
+
+  public Dynamic(String name, int age, String gender)
+  {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 public static void main(String[]args)
   {
   // create an Object for LinkedList
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+
+  LinkedList<Dynamic> p = new LinkedList<>();
+  p.add(new Dynamic("sujatha", 21, "Female"));
+  p.add(new Dynamic("sam", 20,"Male"));
+  p.add(new Dynamic("sravya", 21,"Female"));
+
+  ListIterator<Dynamic> iterator = p.listIterator();
+  while (iterator.hasNext()) {
+      Dynamic person = iterator.next();
+      System.out.println("Name: " + person.name + ", Age: " + person.age + ", Gender: " + person.gender);
+  }
   }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,20 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
+
+    LinkedList<Integer> s = new LinkedList<>();
+        s.add(5);
+        s.add(91);
+        s.add(13);
+        Iterator i = s.iterator();
+         for (Integer values : s) {
+          System.out.println(values);
+
+        }
   }}
