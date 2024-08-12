5a774785018dd1e95b5d11e04fb6d89fcdc3171c
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-12 19:34:39 +0530

    Swarupa

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
+LinkedList<Integer> obj=new LinkedList<>();
+
   // Add Values in it
+obj.add(1);
+obj.add(2);
+obj.add(3);
+obj.add(4);
   // Iterate it by using Iterator
+  Iterator a=obj.iterator();
+  while(a.hasNext()){
+    System.out.println(a.next());
+  }
 }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,9 +1,29 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Dynamic{
 public static void main(String[]args)
   {
   // create an Object for LinkedList
+  LinkedList<String> p1=new LinkedList<>();
+  LinkedList<Integer> p=new LinkedList<>();
     // add person objects in it
+    p1.add("Swarupa");
+    p1.add("Sujatha");
+    p1.add("Kavitha");
+    p1.add("Mahi");
+
     // iterate that List using iterator
+    Iterator person=p1.iterator();
+
     // print it by using while loop
+    while (person.hasNext()) {
+        
+        System.out.println(person.next());
+    }
+   
   }
 }
+
+
