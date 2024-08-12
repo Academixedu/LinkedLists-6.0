784e25819609ceb5a3da997512d420a330e3dcda
psvsai <pasupuletisvsm245@gmail.com>
2024-08-12 21:12:32 +0530

    PSVSAI

--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Dynamic{
 public static void main(String[]args)
   {
@@ -5,5 +8,32 @@ public static void main(String[]args)
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
-  }
+    LinkedList<Person> a= new LinkedList<>();
+
+   
+    a.add(new Person("MAHESH", 30));
+    a.add(new Person("sai", 25));
+    
+
+  
+    Iterator<Person> i = a.iterator();
+    while (i.hasNext()) {
+      
+        System.out.println(i.next());
+    }
 }
+}
+class Person {
+  String name;
+  int age;
+
+  Person(String name, int age) {
+      this.name = name;
+      this.age = age;
+
+  }
+  public String toString() {
+    return  name+age;
+}
+  }
+
