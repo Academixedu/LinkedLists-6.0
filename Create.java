bb006749ed1c739d580b470d7f27202b1ab4a5f1
Shreyash000007 <shreyasharg@gmail.com>
2024-08-12 19:17:24 +0530

    Shreyash

--- Create.java
+++ Create.java
@@ -1,7 +1,33 @@
+import java.util.*;
 public class Create{
+
+  public void meth()
+  {
+    System.out.println("Implementing List =>");
+    LinkedList <Object>in=new LinkedList<Object>();
+    in.add("Java");
+    in.add(10);
+    in.add(0, "First Index");
+    in.add(null);
+    in.add(50);
+    in.add(25);
+
+    System.out.println("Retrieving the data using Iterator interface => ");
+    Iterator <Object>aobj=in.iterator();
+    System.out.println("Retrieving the data in Forward Direction => ");
+    while (aobj.hasNext()) 
+    {
+      System.out.println(aobj.next()); 
+    }
+
+
+
+  }
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  new Create().meth();
+
 }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,9 +1,35 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Dynamic{
+  public void meth1()
+  {
+    System.out.println("Implementing List =>");
+    LinkedList <Person> ll=new LinkedList<>();
+    Person obj1=new Person("Shreyash","Nagpur",22,100000);
+    Person obj2=new Person("Ratan","Nagpur",22,10);
+
+    ll.add(obj1);
+    ll.add(obj2);
+    ll.add(new Person("Sarvesh","Nagpur",22,5));
+
+    Iterator <Person> it=ll.iterator();
+    while (it.hasNext()) 
+    {
+     System.out.println(it.next()); 
+    }
+
+
+  }
+
 public static void main(String[]args)
   {
   // create an Object for LinkedList
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+    Dynamic aobj = new Dynamic();
+    aobj.meth1();
+
   }
 }
