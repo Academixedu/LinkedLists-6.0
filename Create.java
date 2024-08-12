aa84a0012b0a059b7b74644f464f63ba7e9dd4ad
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-12 20:27:08 +0530

    linkedlist

--- Create.java
+++ Create.java
@@ -1,7 +1,18 @@
+import java.util.LinkedList;
+import java.util.Iterator;
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
+  LinkedList<Integer> li=new LinkedList<>();
   // Add Values in it
+  li.add(1);
+  li.add(2);
+  li.add(3);
+  li.add(4);
   // Iterate it by using Iterator
+  Iterator i=li.iterator();
+  while(i.hasNext()){
+      System.out.println(i.next());
+  }
 }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,17 @@
+import java.util.LinkedList;
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
+  LinkedList<Integer> s=new LinkedList<>();
+
     // Add Values
+    s.add(2);
+    s.add(5);
+    s.add(10);
+    for(int i :s){
+      System.out.println(i);
+    }
     // iterate it by using For Each
   }}
+  
+
