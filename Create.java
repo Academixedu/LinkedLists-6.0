99c87f19125b0333e4da320fa4948d75f529ed86
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-12 18:02:10 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,7 +1,24 @@
-public class Create{
-public static void main(String[]args){
-// create  a Linked List class object
-  // Add Values in it
-  // Iterate it by using Iterator
-}
-}
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class prac{
+    public static void main(String[]args){
+    // create  a Linked List class object
+      // Add Values in it
+      // Iterate it by using Iterator
+      LinkedList<Integer> s =new LinkedList<>();
+      s.add(1);
+      s.add(3);
+      s.add(8);
+      s.add(7673);
+      Iterator i=s.iterator();
+      while (i.hasNext()) {
+        System.out.println(i.next());
+        
+      }
+
+
+
+
+    }
+    }
