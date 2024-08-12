36d86d64d699718ec5821d640e886a86116220d2
ratan-17 <rkmule2002@gmail.com>
2024-08-12 18:58:55 +0530

    Ratan

--- ForL.java
+++ ForL.java
@@ -1,6 +1,28 @@
-public class ForL{
-  public static void main(String[]args){
+import java.util.LinkedList;
+
+public class ForL
+{
+
+  void meth()
+  {
+      LinkedList <Object> ll=new LinkedList<>();
+
+      ll.add("Java");
+      ll.add("is");
+      ll.add("Awesome");
+      ll.add(0,10);
+      ll.add(69);
+
+    
+      for (Object obj  :ll) 
+      {
+        System.out.print(obj+",");
+      }
+  }
+  public static void main(String[]args)
+  {
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
+     new ForL().meth();
   }}
