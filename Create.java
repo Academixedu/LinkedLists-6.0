c9e69d2e7738b638fbf67f5228e57eff5bfb57c2
racharlabhanu <racharlabhanu143@gmail.com>
2024-08-28 19:50:58 +0530

    add

--- Create.java
+++ Create.java
@@ -1,7 +1,20 @@
+import java.util.*;
 public class Create{
+
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  int[]a=new int[4];
+  List<Integer> l=new LinkedList<Integer>();
+  l.add(1);
+  l.add(2);
+  l.add(3);
+  l.add(4);
+  Iterator i=l.iterator();
+  while(i.hasNext()){
+    System.out.println(i.next());
+  }
+
 }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,18 @@
+import java.util.*;
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
-  }}
+    List<String> l=new LinkedList<String>();
+    l.add("banana");
+    l.add("apple");
+    l.add("grapes");
+    for(String fruit:l){
+        System.out.println(fruit);
+    }
+
+  
+  
+  }
+}
