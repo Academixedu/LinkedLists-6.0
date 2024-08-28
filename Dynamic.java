ee6e9f90f9f525c85d5474dacbfdb36c2c0d67cd
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-08-28 15:02:20 +0530

    Revathi

--- Dynamic.java
+++ Dynamic.java
@@ -1,9 +1,14 @@
+import java.util.*;
 public class Dynamic{
-public static void main(String[]args)
-  {
-  // create an Object for LinkedList
-    // add person objects in it
-    // iterate that List using iterator
-    // print it by using while loop
+public static void main(String[]args) {
+    List<Person> l=new LinkedList<>();
+    l.add(new Person(20, "V Revathi", 'F'));
+    Iterator<Person> iterator =l.iterator();
+    while(iterator.hasNext()){
+      Person p=iterator.next();
+      System.out.println("Age: "+p.getAge());
+      System.out.println("Name: "+p.getName());
+      System.out.println("Gender: "+p.getGen());
+    }
   }
 }
