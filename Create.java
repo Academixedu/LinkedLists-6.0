22e7c12087a53db9205ed91caf44469e3a62a016
kandgule-dinesh <dineshkandagule@gmail.com>
2024-08-28 13:38:45 +0530

    dinesh

--- Create.java
+++ Create.java
@@ -1,7 +1,18 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  LinkedList<Integer>list = new LinkedList<Integer>();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  Iterator<Integer> i = list.iterator();
+  while (i.hasNext()) {
+    System.out.println(i.next());
+  }
 }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,17 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
-  }}
+    LinkedList<Integer>no = new LinkedList<Integer>();
+    no.add(1);
+    no.add(2);
+    no.add(3);
+    for (Integer i : no) {
+      System.out.println(i);
+    }
+  }
+}
