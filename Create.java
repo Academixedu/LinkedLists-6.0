c65c067cb6865a61fb577bee2bfc6ddade2211bf
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-28 18:51:30 +0530

    create

--- Create.java
+++ Create.java
@@ -1,7 +1,16 @@
+import java.util.*;
 public class Create{
 public static void main(String[]args){
 // create  a Linked List class object
   // Add Values in it
   // Iterate it by using Iterator
+  List<Integer> l=new LinkedList<Integer>();
+  l.add(1);
+  l.add(2);
+  
+  Iterator t=ls.iterator();
+  while(t.hasNext()){
+    System.out.println(t.next());
+  }
 }
 }
