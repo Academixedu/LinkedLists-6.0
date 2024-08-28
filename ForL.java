a359b3e37032e72351962d8de3c98554952456f0
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-08-28 19:13:30 +0530

    Update ForL.java

--- ForL.java
+++ ForL.java
@@ -1,6 +1,20 @@
+package LinkedLists;
+
+import java.util.LinkedList;
+
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
+
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    for(int i = 1; i <= 10; i++){
+        list.add(i);
+    }
+
+    for(int i : list){
+        System.out.println(i);
+    }
+
   }}
