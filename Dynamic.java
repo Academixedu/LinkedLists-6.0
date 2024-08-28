3fdecd249de95cd11cf5d0752daa0e1ce4b2b4fc
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-28 11:32:07 +0530

    done

--- Dynamic.java
+++ Dynamic.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Dynamic{
 public static void main(String[]args)
   {
-    List <Person>k=new LinkedList<Person>();
+    List <person1>k=new LinkedList<person1>();
   
-  k.add(new Person(1, "Rohit", 3465));
-  k.add(new Person(1, "mohit", 321456));
-  k.add(new Person(1, "nanda", 786578426));
-  k.add(new Person(1, "surendra", 326745765));
+  k.add(new person1(1, "Rohit", 3465));
+  k.add(new person1(1, "mohit", 321456));
+  k.add(new person1(1, "nanda", 786578426));
+  k.add(new person1(1, "surendra", 326745765));
 
-   Iterator<Person> l=k.iterator();
+   Iterator<person1> l=k.iterator();
    while (l.hasNext()) {
     System.out.println(l.next());
     
--- person1.java
+++ person1.java
@@ -1,3 +1,33 @@
 public class person1 {
-    
+    private int srno;
+    private String name;
+    private double salary;
+    public int getSrno() {
+        return srno;
+    }
+    public void setSrno(int srno) {
+        this.srno = srno; 
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public void Person1(int srno, String name, double salary) {
+        this.srno = srno;
+        this.name = name;
+        this.salary = salary;
+    }
+   
+    @Override
+    public String toString() {
+        return "Person [srno=" + srno + ", name=" + name + ", salary=" + salary + "]";
+    }
 }
