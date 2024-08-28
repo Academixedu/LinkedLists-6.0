410a4ccc07bb4351266124e59d2bcfe6694bdaa5
Aparna526 <aparnakondapaturu@gmail.com>
2024-08-28 12:07:53 +0530

    one

--- Dynamic.java
+++ Dynamic.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Dynamic{
 public static void main(String[]args)
   {
@@ -5,5 +8,16 @@ public static void main(String[]args)
     // add person objects in it
     // iterate that List using iterator
     // print it by using while loop
+
+    LinkedList<Person> l = new LinkedList<>();
+    l.add(new Person(1,"Salaar", 100000.250));
+    l.add(new Person(2,"Kalki", 200000.250));
+
+    Iterator<Person> i = l.iterator();
+
+    while (i.hasNext()) {
+      System.out.println(i.next());
+      
+    }
   }
 }
--- ForL.java
+++ ForL.java
@@ -1,6 +1,19 @@
+import java.util.LinkedList;
+
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
-  }}
+
+    LinkedList<String> l = new LinkedList<>();
+    l.add("Apple");
+    l.add("Mango");
+    l.add("Grapes");
+    l.add("Pineapple");
+
+    for(String p : l){
+      System.out.println(p);
+    }
+  }
+}
