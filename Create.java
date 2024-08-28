0883908592c2c1e5e903674d2817ea334c063ddb
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-28 19:01:46 +0530

    save

--- Create.java
+++ Create.java
@@ -1,7 +1,61 @@
-public class Create{
-public static void main(String[]args){
-// create  a Linked List class object
-  // Add Values in it
-  // Iterate it by using Iterator
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+class LinkedList<T> implements Iterable<T> {
+    private Node<T> head;
+
+    // Node class
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data) { this.data = data; }
+    }
+
+    // Add value to the linked list
+    public void add(T value) {
+        if (head == null) {
+            head = new Node<>(value);
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(value);
+        }
+    }
+
+    // Iterator implementation
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        for (int value : list) {
+            System.out.println(value);
+        }
+    }
 }
-}
+
