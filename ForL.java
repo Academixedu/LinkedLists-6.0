59140541007990d9983684eeaaada4149229ae70
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-28 11:34:11 +0530

    done

--- ForL.java
+++ ForL.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ForL{
   public static void main(String[]args){
+
+     List<String> b=new LinkedList<String>();
+        
+        b.add("a");
+        b.add("c");
+        b.add("h");
+
+        b.add("g");
+        b.add("j");
+
+        for(String a:b){
+          System.out.println(a);
+        }
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
