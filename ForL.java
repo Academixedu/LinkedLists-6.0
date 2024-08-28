0c5f3b8027668660a578e10d8c4b6fef0671953c
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-08-28 11:39:47 +0530

    shahid

--- ForL.java
+++ ForL.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForL{
   public static void main(String[]args){
   // create a Linked List Object
     // Add Values
     // iterate it by using For Each
+      List<String> b=new ArrayList<String>();
+  b.add("w");
+  b.add("ww");
+  b.add("www");
+  b.add("wwwww");
+  b.add("wwwwww");
+  b.add("wwwwwwwww");
+ 
+  for(String n:b){
+    System.err.println(n);
+  }
+
   }}
